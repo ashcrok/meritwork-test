abac6508403fd39b659b3282dbafe9d4ec07c8f3
ashcrok <mihai.t.pricop@gmail.com>
2014-10-21 15:27:05 +0300

    design

--- html/js/controllers/MainCtrl.js
+++ html/js/controllers/MainCtrl.js
@@ -6,19 +6,22 @@ myApp.controller('MainCtrl', function ($scope, $rootScope, $window, $location) {
     var user1 = {
         'name'      : 'vasile',
         'avatar'    : 'http://png-4.findicons.com/files/icons/2443/bunch_of_cool_bluish_icons/512/user.png',
-        'service'   : 'programmer'
+        'service'   : 'programmer',
+        'degree'    : '2nd'
     }
 
     var user2 = {
         'name'      : 'andrei',
         'avatar'    : 'http://png-4.findicons.com/files/icons/2443/bunch_of_cool_bluish_icons/512/user.png',
-        'service'   : 'programmer'
+        'service'   : 'programmer',
+        'degree'    : '1st'
     }
 
     var user3 = {
         'name'      : 'radu',
         'avatar'    : 'http://png-4.findicons.com/files/icons/2443/bunch_of_cool_bluish_icons/512/user.png',
-        'service'   : 'programmer'
+        'service'   : 'programmer',
+        'degree'    : '4th'
     }
 
     $scope.users = [user1, user2, user3];
